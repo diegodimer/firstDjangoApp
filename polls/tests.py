5f3214e0c548669a061fcfc09711724cee6581db
Diego Dimer <diego.dimer@ufrgs.br>
2019-07-13 22:47:07 -0300

    tests for detailView

--- polls/tests.py
+++ polls/tests.py
@@ -77,3 +77,22 @@ class QuestionIndexViewTests(TestCase):
                 response.context['latest_question_list'],
                 ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+        
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        questions with a pub_date in the future can not be accessed 
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        
+    def test_past_question(self):
+        """
+        questions with a pub_date in the past can be accessed
+        """
+        past_question = create_question(question_text="Past question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
